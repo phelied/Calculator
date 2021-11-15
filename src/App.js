b256efebc09ff40ba36825e38d38c72b1249f7f6
kuueli <kuu@MacBook-Pro-de-kuueli.local>
2021-11-15 18:51:51 +0100

    add Css

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Container className='container'>
           <StylingCellsGrid />
-        </Container>
       </header>
     </div>
   );
--- src/components/utilities/utilities.js
+++ src/components/utilities/utilities.js
@@ -6,25 +6,17 @@ import Button from '@mui/material/Button';
 
 const rows = ['(', ')', '%', 'AC', '7', '8', '9', '/', '4', '5', '6', 'x', '1', '2', '3', '-', '0', '.', '=', '+'];
 
-const useStyles = makeStyles({
-    root: {
-        '& .super-app-theme--cell': {
-            backgroundColor: 'rgba(224, 183, 60, 0.55)',
-            color: '#1a3e72',
-            fontWeight: '900',
-        },
-    },
-});
-
-const Demo = ({ results}) => (
-    <div>
+const Demo = ({ results }) => (
+    <div className="background-calculator">
+        <div className="results">p</div>
         {results.length > 0 && (
             Array.from({ length: Math.ceil(results.length / 4) }, (_, i) => (
                 <div className="row" key={`row${i}`}>
+
                     {
                         results.slice(i * 4, (i + 1) * 4)
                             .map((number) => (
-                                <Button className="card" key={number}>{number}</Button>
+                                <button className="background-button" key={number}>{number}</button>
                             ))
                     }
                 </div>
@@ -34,10 +26,9 @@ const Demo = ({ results}) => (
 )
 
 export default function StylingCellsGrid() {
-    const classes = useStyles();
     return (
-        <div style={{ height: 200, width: '100%' }} className={classes.root}>
-                <Demo results={rows} />
+        <div className="center">
+            <Demo results={rows} />
         </div>
     );
 }

0948899c498760b18957c14c3f1d98b01ac64a29
kuueli <kuu@MacBook-Pro-de-kuueli.local>
2021-11-15 23:28:41 +0100

    Show only number

--- src/components/utilities/utilities.js
+++ src/components/utilities/utilities.js
@@ -7,30 +7,34 @@ const rows = ['(', ')', '%', 'AC', '7', '8', '9', '/', '4', '5', '6', 'x', '1', 
 
 function Demo({ results }) {
     const [stringData, setStringData] = useState("");
+    const [number, setNumber] = useState("0");
     const handleClick = (e) => {
         console.log(e.target.value);
+        if (!isNaN(e.target.value)) {
+            setNumber(e.target.value);
+        }
         switch (e.target.value) {
             case 'AC':
-                setStringData(""); 
-              break;
+                setStringData("");
+                break;
             case '=':
-                setStringData(eval(stringData));
+                setNumber(eval(stringData));
                 break;
             default:
-                setStringData(stringData + e.target.value); 
-          }
+                setStringData(stringData + e.target.value);
+        }
     }
 
     return (
         <div className="background-calculator">
-            <div className="results">{stringData ? stringData : 0}</div>
+            <div className="results">{number}</div>
             {results.length > 0 && (
                 Array.from({ length: Math.ceil(results.length / 4) }, (_, i) => (
                     <div className="row" key={`row${i}`}>
                         {
                             results.slice(i * 4, (i + 1) * 4)
                                 .map((character) => (
-                                    <button style={{ backgroundColor: character === "=" ? '#8ab4f9' : '' }} className="background-button" value={character === "x" ? "*": character} key={character} onClick={handleClick}> {character} </button>
+                                    <button style={{ backgroundColor: character === "=" ? '#8ab4f9' : '' }} className="background-button" value={character === "x" ? '*' : character} key={character} onClick={handleClick}> {character} </button>
                                 ))
                         }
                     </div>

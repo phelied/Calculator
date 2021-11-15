f194ee376899ce5b8242ccc1ded1d8477d27b3fc
kuueli <kuu@MacBook-Pro-de-kuueli.local>
2021-11-15 22:09:04 +0100

    Result frame

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { Container } from '@mui/material';
 import StylingCellsGrid from './components/utilities/utilities';
+import React from 'react';
 
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
--- src/components/utilities/utilities.js
+++ src/components/utilities/utilities.js
@@ -3,18 +3,24 @@ import './utilities.css';
 
 const rows = ['(', ')', '%', 'AC', '7', '8', '9', '/', '4', '5', '6', 'x', '1', '2', '3', '-', '0', '.', '=', '+'];
 
+
+
 function Demo({ results }) {
+    const [stringData, setStringData] = useState("");
+    const handleClick = (e) => {
+        setStringData(stringData + e.target.value);
+      }
 
     return (
         <div className="background-calculator">
-            <div className="results">0</div>
+            <div className="results">{stringData ? stringData : 0}</div>
             {results.length > 0 && (
                 Array.from({ length: Math.ceil(results.length / 4) }, (_, i) => (
                     <div className="row" key={`row${i}`}>
                         {
                             results.slice(i * 4, (i + 1) * 4)
                                 .map((character) => (
-                                    <button style = {{ backgroundColor : character == "=" ? '#8ab4f9' : ''}} className="background-button" key={character}> {character} </button>
+                                    <button style = {{ backgroundColor : character === "=" ? '#8ab4f9' : ''}} className="background-button" value={character} key={character} onClick={handleClick}> {character} </button>
                                 ))
                         }
                     </div>
